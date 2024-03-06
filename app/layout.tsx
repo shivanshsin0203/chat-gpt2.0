37fcf9927de2a5f989ba8404d876229300f8d995
Shivansh Singh <singhshivansh12may@gmail.com>
2024-03-06 13:32:37 +0530

    Starting Home Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className=" flex">
+          <div className=" flex-1 bg-[#343541]">
+        {children}
+        </div>
+        </div>
+        
+        </body>
     </html>
   );
 }

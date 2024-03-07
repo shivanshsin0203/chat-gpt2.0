f0cf0e3c94ccab26246d2a2aaa9d67e03ea863e5
Shivansh Singh <singhshivansh12may@gmail.com>
2024-03-07 18:43:56 +0530

    Added next auth

---
+++ components/SessionProvider.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import exp from "constants"
+import { Session } from "next-auth"
+import { SessionProvider as Provider } from "next-auth/react"
+
+type Props = {
+    session: Session|null
+    children: React.ReactNode
+    }
+export default function SessionProvider({  children,session }: Props) {
+    return <Provider >{children}</Provider>
+}

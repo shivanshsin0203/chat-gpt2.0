fdc28b220a39fada26ae507c91c2e9f4fdf52b80
Shivansh Singh <singhshivansh12may@gmail.com>
2024-03-08 20:26:19 +0530

    Minor Changes

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -3,7 +3,7 @@ import { PlusIcon } from "@heroicons/react/24/solid"
 const NewChat = () => {
   return (
     <div className=" chatrow border-gray-700 border">
-      <PlusIcon className=" w-8 h-8"/>
+      <PlusIcon className=" w-8 h-8 "/>
       <p>New Chat</p>
     </div>
   )

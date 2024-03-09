c3ae41f51a6d18b00e18cafe42a5bd91c8b3de64
Shivansh Singh <singhshivansh12may@gmail.com>
2024-03-09 10:27:31 +0530

    Added Logout

--- components/Login.tsx
+++ components/Login.tsx
@@ -3,14 +3,14 @@ import { signIn } from "next-auth/react"
 import Image from "next/image"
 const Login = () => {
   return (
-    <div>
+    <div className=" bg-[#11A37F] flex flex-col items-center justify-center h-screen text-center">
         <Image
         src={'https://links.papareact.com/2i6'}
         width={300}
         height={300}
         alt="logo"
          />
-         <button>Sign In to ChatGpt</button>
+         <button onClick={()=>signIn('google')} className=" text-white font-bold text-3xl animate-pulse cursor-pointer">Sign In to ChatGpt</button>
     </div>
   )
 }
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,6 +1,9 @@
+'use client'
+import { useSession,signOut } from "next-auth/react"
 import NewChat from "./NewChat"
 
 const Sidebar = () => {
+    const {data:session}=useSession()
   return (
     <div className=" p-2 flex flex-col h-screen ">
         <div className=" flex-1">
@@ -14,7 +17,7 @@ const Sidebar = () => {
                 {/* Chat Rows */}
             </div>
         </div>
-        
+        {session && <img onClick={()=>signOut()} src={session.user?.image!} alt="user" className=" w-12 h-12 rounded-full cursor-pointer hover:opacity-50 mx-auto mb-2 " />}
         </div>
   )
 }

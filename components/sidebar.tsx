15a27078c3bbab83cc3d27b97cd6dfeace6a8402
Shivansh Singh <singhshivansh12may@gmail.com>
2024-03-10 10:21:15 +0530

    Added React Hooks

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,9 +1,14 @@
 'use client'
 import { useSession,signOut } from "next-auth/react"
 import NewChat from "./NewChat"
-
+import {useCollection} from "react-firebase-hooks/firestore"
+import { collection } from "firebase/firestore"
+import { db } from "@/firebase"
+import ChatRow from "./ChatRow"
 const Sidebar = () => {
     const {data:session}=useSession()
+    const [chats,loading,error]=useCollection(session && session.user && typeof session.user.email === 'string' ? collection(db,"users",session.user.email,"chats") : null);
+    console.log(chats)
   return (
     <div className=" p-2 flex flex-col h-screen ">
         <div className=" flex-1">
@@ -14,7 +19,9 @@ const Sidebar = () => {
                 {/* Models */}
             </div>
             <div>
-                {/* Chat Rows */}
+                {chats?.docs.map(chat=>(
+                    <ChatRow key={chat.id} id={chat.id}/>
+                ))}
             </div>
         </div>
         {session && <img onClick={()=>signOut()} src={session.user?.image!} alt="user" className=" w-12 h-12 rounded-full cursor-pointer hover:opacity-50 mx-auto mb-2 " />}
